69db7377290772c4e87c34b43b11962eea175279
innoflash <innocentmazando@gmail.com>
2022-08-11 08:58:47 +0200

    added panics home

--- src/app/panics/panics-routing.module.ts
+++ src/app/panics/panics-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '',
+    component: HomeComponent
+}];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class PanicsRoutingModule { }
+export class PanicsRoutingModule {
+}
--- src/app/panics/panics.module.ts
+++ src/app/panics/panics.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PanicsRoutingModule } from './panics-routing.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
     PanicsRoutingModule
