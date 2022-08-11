a0dfe0822a16635e5b12103d0596707d7330b8d8
innoflash <innocentmazando@gmail.com>
2022-08-11 09:23:12 +0200

    wip: error interceptor

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { NotificationService } from "../services/notification.service";
+import { NotificationType } from "../config/app.types";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+    public constructor(private readonly notificationService: NotificationService) {
+    }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
-  }
+    public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        return next.handle(request).pipe(
+            catchError((err: HttpErrorResponse) => {
+                console.log({ err });
+                this.notificationService.show(err.message, NotificationType.ERROR);
+                return throwError(() => err.error);
+            })
+        );
+    }
 }
